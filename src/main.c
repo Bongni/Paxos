57999fa153750e170b3a6ac27667e16c3b50ae04
Bongni <s.bongni@bluewin.ch>
2024-01-28 23:18:27 +0100

    Added debug flag.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool DEBUG_FLAG = false;
+
+void set_flags(char *argv[], int argc) {
+    if ( strcmp(argv[1], "-d") == 0 ) {
+        DEBUG_FLAG = true;
+        printf("Debugging enabled.\n");
+    }
+}
 
 int main (int argc, char *argv[]) {
 
-    printf("Hello there!\n");
+    printf("Executing Paxos ... \n");
+
+    if (argc >= 2) {
+        set_flags(argv, argc);
+    }
 
     return 0;
 }
