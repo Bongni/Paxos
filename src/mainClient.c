621f55b22b7cdd1e78db3a85a3eaf90e727c9eb1
Bongni <s.bongni@bluewin.ch>
2024-02-01 14:45:13 +0100

    Small working client/server pair.

--- src/mainClient.c
+++ src/mainClient.c
@@ -1,5 +1,42 @@
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
+#define PORT 8080
 
-int main() {
+int main(int argc, char *argv) {
+    
+    int client_fd;
+    struct sockaddr_in server_addr;
+    
+    if((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+
+    if(inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+        printf("\nInvalid address\n");
+        return -1;
+    }
+
+    int status;
+
+    if((status = connect(client_fd, (struct sockaddr*) &server_addr, sizeof(server_addr))) < 0) {
+        printf("\nConnection failed\n");
+        return -1;
+    }
+
+    char *message = "Test!";
+    send(client_fd, message, strlen(message), 0);
+    printf("Message sent\n");
+
+    close(client_fd);
     return 0;
 }
--- src/mainServer.c
+++ src/mainServer.c
@@ -1,5 +1,54 @@
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
 
+#define PORT 8080
 
-int main() {
+int main(int argc, char *argv) {
+    
+    int server_fd;
+    struct sockaddr_in address;
+    
+    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if(bind(server_fd, (struct sockaddr*) &address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if(listen(server_fd, 5) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+        Accept incoming connections
+    */
+
+    int client_fd;
+    socklen_t client_addrlen;
+    char message[1024] = { 0 };
+
+    while (true) {
+        if((client_fd = accept(server_fd, (struct sockaddr*) &address, &client_addrlen)) < 0) {
+            perror("accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        read(client_fd, message, 1024 - 1);
+
+        printf("%s\n", message);
+    }
+    
     return 0;
 }
